8c18672e7ab64dd679ca636cdca5eed6952d32e3
Oleg <you@example.com>
2024-10-21 00:51:14 +0400

    refactored project

--- src/main/kotlin/org/oleg/iem/services/lmm/LlmService.kt
+++ src/main/kotlin/org/oleg/iem/services/lmm/LlmService.kt
@@ -4,7 +4,10 @@ import com.intellij.openapi.components.Service
 import com.intellij.openapi.project.Project
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
+import org.oleg.iem.listeners.LlmRequestProcessedListener
 import org.oleg.iem.listeners.LlmRequestReceivedListener
+import org.oleg.iem.listeners.LlmResponseReadyListener
+import org.oleg.iem.utils.AskLLMResponse
 
 @Service(Service.Level.PROJECT)
 // TODO: Rewrite code according to the community recommendations
@@ -16,6 +19,18 @@ class LlmService (
         cs.launch {
             println("Running coroutine to get LLM response")
             LlmUtils().prepareRequest(request)
+
+            val requestPublisher: LlmRequestProcessedListener = project.messageBus
+                .syncPublisher(LlmRequestProcessedListener.LLM_REQUEST_PROCESSED_TOPIC)
+            requestPublisher.requestProcessed(request)
+
+            val client = LlmClient()
+            val stringResponse = client.queryLLM(request.model, request.prompt!!)
+            val response = AskLLMResponse(request.prompt, request.promptTemplate, request.query, stringResponse)
+
+            val responsePublisher: LlmResponseReadyListener = project.messageBus
+                .syncPublisher(LlmResponseReadyListener.LLM_RESPONSE_READY_TOPIC)
+            responsePublisher.responseReceived(response)
         }
     }
 
