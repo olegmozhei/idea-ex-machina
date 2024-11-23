24f3ccfea46d161c8909ab8e787766538918529c
Oleg <you@example.com>
2024-11-23 19:55:55 +0400

    first step to bring customization

---
+++ src/main/java/org/oleg/iem/my_prompt_language/MyPromptIcons.java
@@ -0,0 +1,9 @@
+package org.oleg.iem.my_prompt_language;
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+
+public class MyPromptIcons {
+    public static final Icon FILE = IconLoader.getIcon("/icons/file_icon.png", MyPromptIcons.class);
+}
