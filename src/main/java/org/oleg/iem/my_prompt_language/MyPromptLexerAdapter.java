526482fd484da9605c8fd796c10f143721d8222f
Oleg <you@example.com>
2024-11-26 20:06:22 +0400

    autogenerated code

--- src/main/java/org/oleg/iem/my_prompt_language/MyPromptLexerAdapter.java
+++ src/main/java/org/oleg/iem/my_prompt_language/MyPromptLexerAdapter.java
@@ -3,6 +3,7 @@ package org.oleg.iem.my_prompt_language;
 import com.intellij.lexer.FlexAdapter;
 import com.intellij.lexer.FlexLexer;
 import org.jetbrains.annotations.NotNull;
+import org.oleg.iem.my_prompt_language.gen.GeneratedMyPromptLexer;
 
 public class MyPromptLexerAdapter extends FlexAdapter {
 
--- src/main/java/org/oleg/iem/my_prompt_language/gen/psi/impl/MyPromptQueryImpl.java
+++ src/main/java/org/oleg/iem/my_prompt_language/gen/psi/impl/MyPromptQueryImpl.java
@@ -1,13 +1,15 @@
 // This is a generated file. Not intended for manual editing.
 package org.oleg.iem.my_prompt_language.gen.psi.impl;
 
+import java.util.List;
 import org.jetbrains.annotations.*;
 import com.intellij.lang.ASTNode;
+import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiElementVisitor;
+import com.intellij.psi.util.PsiTreeUtil;
+import static org.oleg.iem.my_prompt_language.gen.psi.MyPromptTypes.*;
 import com.intellij.extapi.psi.ASTWrapperPsiElement;
-import org.oleg.iem.my_prompt_language.gen.psi.MyPromptQuery;
-import org.oleg.iem.my_prompt_language.gen.psi.MyPromptVisitor;
-import org.oleg.iem.my_prompt_language.psi.*;
+import org.oleg.iem.my_prompt_language.gen.psi.*;
 
 public class MyPromptQueryImpl extends ASTWrapperPsiElement implements MyPromptQuery {
 
